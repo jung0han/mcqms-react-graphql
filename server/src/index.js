bb126bfe4ab263f3acf23a0547e910b928374707
jung0han <jung0han@hanmail.net>
2020-12-28 14:39:28 +0900

    init

---
+++ server/src/index.js
@@ -0,0 +1,64 @@
+const { ApolloServer, PubSub } = require('apollo-server');
+const { PrismaClient } = require('@prisma/client');
+const Subscription = require('./resolvers/Subscription')
+
+const pubsub = new PubSub();
+
+
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+  }]
+  
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    // 2
+    feed: async (parent, args, context) => {
+      return context.prisma.link.findMany()
+    },
+  },
+  Mutation: {
+    post: (parent, args, context, info) => {
+      const newLink = context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      })
+      return newLink
+    },
+  },
+  Subscription
+}
+
+const fs = require('fs');
+const path = require('path');
+
+const prisma = new PrismaClient()
+
+const server = new ApolloServer({
+  typeDefs: fs.readFileSync(
+    path.join(__dirname, 'schema.graphql'),
+    'utf8'
+  ),
+  resolvers,
+  context: ({ req }) => {
+    return {
+      ...req,
+      prisma,
+      pubsub,
+      userId:
+        req && req.headers.authorization
+          ? getUserId(req)
+          : null
+    };
+  }
+})
+
+server
+  .listen()
+  .then(({ url }) =>
+    console.log(`Server is running on ${url}`)
+  );
