53aabbd3a72ca21ad9fd611337a3769a66443ded
jung0han <jung0han@hanmail.net>
2020-12-29 10:37:08 +0900

    add vote model

--- server/src/index.js
+++ server/src/index.js
@@ -1,64 +1,40 @@
-const { ApolloServer, PubSub } = require('apollo-server');
-const { PrismaClient } = require('@prisma/client');
-const Subscription = require('./resolvers/Subscription')
+const { ApolloServer, PubSub } = require("apollo-server");
+const { PrismaClient } = require("@prisma/client");
+const Subscription = require("./resolvers/Subscription");
+const { getUserId } = require("./utils");
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const User = require("./resolvers/User");
+const Link = require("./resolvers/Link");
+const Vote = require("./resolvers/Vote");
 
 const pubsub = new PubSub();
 
-
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-  }]
-  
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    // 2
-    feed: async (parent, args, context) => {
-      return context.prisma.link.findMany()
-    },
-  },
-  Mutation: {
-    post: (parent, args, context, info) => {
-      const newLink = context.prisma.link.create({
-        data: {
-          url: args.url,
-          description: args.description,
-        },
-      })
-      return newLink
-    },
-  },
-  Subscription
-}
+  Query,
+  Mutation,
+  Subscription,
+  User,
+  Link,
+  Vote,
+};
 
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 const server = new ApolloServer({
-  typeDefs: fs.readFileSync(
-    path.join(__dirname, 'schema.graphql'),
-    'utf8'
-  ),
+  typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
   resolvers,
   context: ({ req }) => {
     return {
       ...req,
       prisma,
       pubsub,
-      userId:
-        req && req.headers.authorization
-          ? getUserId(req)
-          : null
+      userId: req && req.headers.authorization ? getUserId(req) : null,
     };
-  }
-})
+  },
+});
 
-server
-  .listen()
-  .then(({ url }) =>
-    console.log(`Server is running on ${url}`)
-  );
+server.listen().then(({ url }) => console.log(`Server is running on ${url}`));
