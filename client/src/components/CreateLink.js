c1d9cfe5b250cacf38b25af4f83664d6537f308e
dongwoo.jeong <jung0han@ajou.ac.kr>
2020-12-30 13:55:16 +0900

    add subsrciption function in client

--- client/src/components/CreateLink.js
+++ client/src/components/CreateLink.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { useMutation, gql } from "@apollo/client";
 import { useHistory } from "react-router";
+import { LINKS_PER_PAGE } from "../constants";
+import { FEED_QUERY } from "./LinkList";
 
 const CREATE_LINK_MUTATION = gql`
   mutation PostMutation($description: String!, $url: String!) {
@@ -19,12 +21,41 @@ const CreateLink = () => {
     description: "",
     url: "",
   });
+
   const [createLink] = useMutation(CREATE_LINK_MUTATION, {
     variables: {
       description: formState.description,
       url: formState.url,
     },
-    onCompleted: () => history.push("/"),
+    update: (cache, { data: { post } }) => {
+      const take = LINKS_PER_PAGE;
+      const skip = 0;
+      const orderBy = { createdAt: "desc" };
+
+      const data = cache.readQuery({
+        query: FEED_QUERY,
+        variables: {
+          take,
+          skip,
+          orderBy,
+        },
+      });
+
+      cache.writeQuery({
+        query: FEED_QUERY,
+        data: {
+          feed: {
+            links: [post, ...data.feed.links],
+          },
+        },
+        variables: {
+          take,
+          skip,
+          orderBy,
+        },
+      });
+    },
+    onCompleted: () => history.push("/new/1"),
   });
 
   return (
--- client/src/components/Link.js
+++ client/src/components/Link.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useMutation, gql } from "@apollo/client";
 import { AUTH_TOKEN, LINKS_PER_PAGE } from "../constants";
+import { FEED_QUERY } from "./LinkList";
 import { timeDifferenceForDate } from "../utils";
 
 const VOTE_MUTATION = gql`
@@ -34,6 +35,30 @@ const Link = (props) => {
   const [vote] = useMutation(VOTE_MUTATION, {
     variables: {
       linkId: link.id,
+    },
+    update(cache, { data: { vote } }) {
+      const { feed } = cache.readQuery({
+        query: FEED_QUERY,
+      });
+
+      const updatedLinks = feed.links.map((feedLink) => {
+        if (feedLink.id === link.id) {
+          return {
+            ...feedLink,
+            votes: [...feedLink.votes, vote],
+          };
+        }
+        return feedLink;
+      });
+
+      cache.writeQuery({
+        query: FEED_QUERY,
+        data: {
+          feed: {
+            links: updatedLinks,
+          },
+        },
+      });
     },
   });
 
